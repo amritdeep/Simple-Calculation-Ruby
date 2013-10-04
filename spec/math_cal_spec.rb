d9363692827bb8165b0ba3c6ea6c509641cf52a9
Amritdeep Dhungana <Amritdeep@Amritdeeps-MacBook-Pro.local>
2013-10-03 23:44:42 -0500

    make the code more dry

--- spec/math_cal_spec.rb
+++ spec/math_cal_spec.rb
@@ -1,21 +1,26 @@
 require "./lib/math_cal.rb"
 
 describe Calculation do
-	subject { Calculation.new('a', 'b') }
-	
-	it "add number" do
-		subject.should respond_to :add
-	end
-	it "subtract number" do
-		subject.should respond_to :subtract
+	# subject { Calculation.new('a', 'b') }
 
-	end
-	it "multiple number" do
-		subject.should respond_to :multiple
+	subject do
+		Calculation.new('a', 'b')
 	end
 
-	it "divide number" do
-		subject.should respond_to :divide
+	describe "add function" do
+		it {should respond_to :add}
+	end
+
+	describe "subtract function" do
+		it {should respond_to :subtract}
+	end
+
+	describe "multiple function" do
+		it {should respond_to :multiple}
+	end
+
+	describe "divide function" do
+		it {should respond_to :divide}
 	end
 
 end
